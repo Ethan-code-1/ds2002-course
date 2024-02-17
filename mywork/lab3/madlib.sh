3556b699f9283a24fcec36912a4d9797c6bbee85
Ethan Ogilvie <aftonethan@gmail.com>
2024-02-17 18:45:00 -0500

    Moved lab3 contents to folder

---
+++ mywork/lab3/madlib.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+clear 
+echo "Let's build a mad-lib!"
+
+read -p "1. Please give me an adjective: " ADJ1
+
+read -p "2. Please give me a verb: " VERB1
+
+read -p "3. Please give me a second adjective: " ADJ2
+
+read -p "4. Please give me a noun: " NOUN1
+
+read -p "5. Please give me a second verb: " VERB2
+
+read -p "6. Please give me a third adjective: " ADJ3
+
+read -p "7. Please give me a second noun: " NOUN2
+
+read -p "8. Please give me a fourth adjective: " ADJ4
+
+echo -e "Here is the final product:\n"
+
+echo -e "The food at O Hill is very $ADJ1. When I eat there I feel like I need to $VERB1 because the food is always very $ADJ2. Sometimes the food even taste like $NOUN1. I always make sure to $VERB2 a lot after eating there. The best thing I ever had there was a $ADJ3 burger which I ate with a $NOUN2. With it a drank some soda that was very $ADJ4.\n"
+
